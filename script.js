4ee2a406bfb9f848310b09b6f5097eba57b75fde
Amber Lee <lee.amber.alex@gmail.com>
2020-09-19 13:53:57 -0700

    added working functions for steps 1-5

--- script.js
+++ script.js
@@ -23,50 +23,53 @@ var special1 = ["@" , "#", "$", "%", "^", "&", "*",];
 var numChar1 = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var alpha1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 , "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-console.log(alpha1)
+var alpha2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
+, "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+console.log("alpha1", alpha1)
 
 
-// //  1. Welcome to Password Generator! Click "ok" to continue.
+// // //  1. Welcome to Password Generator! Click "ok" to continue.
       alert("Welcome to Password Generator! Click 'ok' to begin creating your unique password.");
       var charLength = prompt("How many characters would you like your password to contain?");
 
 
 //   //  2. If you would like to include special characters(#@$%&*^!), click "ok".
-      var special2 = confirm("If you would like to include special characters(#@$%&*^!), click 'ok'.")
+      var special2 = confirm("If you would like to include special characters(#@$%&*^!), click 'ok'.");
           if (special2) 
           {
-            for( let i = 0; i < special1.length; i++);{
-            special3= special1.charAt(Math.floor(Math.random() * Math.floor(special1.length-1)));
+            for( let i = 0; i < special1.length; i++){
+              special3= special1[Math.floor(Math.random() * special1.length-1)];
           }
         }
-          console.log(special3);
+          console.log("special3 ", special3);
 
 //   //  3. If you would like to include numeric characters, click "ok".
       var numChar2 = confirm("If you would like to include numeric characters, click 'ok'.");
         if(numChar2){
-          for( let i = 0; i<10; i++);
+          for( let i = 0; i<10; i++){
           var numChar2 = Math.floor(Math.random() * 10);
         }
-        console.log(numChar2);
+      }
+        console.log("numChar2" ,numChar2);
 
   // //  4. If you would like to include lowercase characters, click "ok".
-  //     var lowChar = confirm("If you would like to include lowercase characters, click 'ok'.");
-  //     if(lowChar) {
-  //       for (let i = 0; i < alpha1.length; i++){
-  //       var LowChar1 = alpha1.charAt(Math.floor(Math.random() * alpha1.length));
-  //       }
-
-  //       }
-  //       // lowChar1 = lowChar1[i].toLowerCase} 
-  //       else {
-  //         var LowChar1 = alpha1.charAt(Math.floor(Math.random() * Math.floor(alpha1.length - 1)));
-  //       }
-        
-  //       console.log(lowChar1);
+      var lowChar = confirm("If you would like to include lowercase characters, click 'ok'.");
+      if(lowChar) {
+        for (let i = 0; i < alpha2.length; i++){
+        var lowChar1 = alpha2[Math.floor(Math.random() * alpha2.length-1)];
+        }
+      }
+        console.log("lowChar1", lowChar1);
         
 
   //  5. If you would like to include uppercase characters, click "ok".
-      var uppChar = confirm("f you would like to include uppercase characters, click 'ok'.")
+      var uppChar = confirm("if you would like to include uppercase characters, click 'ok'.");
+      if(uppChar) {
+        for (let i = 0; i < alpha1.length; i++){
+        var uppChar2 = alpha1[Math.floor(Math.random() * alpha1.length-1)];
+        }
+      }
+        console.log("uppChar2", uppChar2);
 
 // var generateBtn = document.querySelector("#generate");
 
