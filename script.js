9a73c5ca992e0814cebf4393f0102d8a336047fc
Amber Lee <lee.amber.alex@gmail.com>
2020-09-23 00:08:48 -0700

    Added images of working page.

--- script.js
+++ script.js
@@ -1,45 +1,86 @@
-//  var lowChar1 = alpha2[Math.floor(Math.random() * alpha2.length-1)];
+// randoSelects: ("@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890");
+// var randoChar1 = randoSelects[Math.floor(Math.random() * randoSelects.length -1)];
+// if(randoSelects.indexOf() !== -1)
 
-var generatePassword = function(){
-    randoSelects = ("@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890");
-    password: function (arr){
-       for (let i = 0; i < charLength.length; i++){
-     };
+// if the confirm is true then set object key to true, and find a way to for loop based on - charLength
+// =========================================================
+var generatePassword = function () {
+  
+  var allChar = [
+      hasspecialChar = ["@", "#", "$", "%", "^", "&", "*"],
+      hasnumChar = ["1","2", "3", "4", "5", "6", "7", "8", "9", "0"],
+      haslowChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+      hasupChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  ];
+
 // STEP 1
-    charLength = prompt("Welcome to Password Generator! How many characters would you like your password to contain?");
-
-// STEP 2
-    special2: function(arr) {confirm("If you would like to include special characters(#@$%&*^!), click 'ok'.");
-    var randoSelects1 = ["@,#,$,%,^,&,*,"];
-          if(special2) {
-          return randoSelects1[Math.floor(Math.random() * randoSelects1.length-1)];
-          }
-          console.log(randoSelects1);
-        };
-// STEP 3
-    numChar2: function(arr) {confirm("If you would like to include numeric characters, click 'ok'.");
-        if(numChar2){
-          for( let i = 0; i<1; i++){
-          var testAnswer2 = Math.floor(Math.random() * 10);
-          return testAnswer2 ; 
-        }
-        console.log(testAnswer2);
-      }
-    }};
-// STEP 4
-    lowChar: function(arr) {confirm("If you would like to include lowercase characters, click 'ok'.");
-      var randoSelects2 = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,rs,t,u,v,w,x,y,z"];
-        if(lowChar){
-          randoSelects.indexOf
-        };
-        
-// STEP 5        
-    uppChar: function(arr) {confirm("if you would like to include uppercase characters, click 'ok'.");
-      var randoSelects3 =["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-            if(uppChar){}
-} 
+var charLength = parseInt(
+  prompt(
+    "Welcome to Password Generator! How many characters would you like your password to contain?"
+  )
+);
+console.log(charLength);
+// validate character length.
+if (isNaN(charLength) === true) {
+  alert("Please provide a number.");
+  return;
+} else if (charLength >= 128) {
+  alert("Please provide a number less than 128.");
+  return;
+} else if (charLength < 8) {
+  alert("Please provide a number greater than 8.");
+  return;
 }
-};
+// STEP 2
+
+var specialChar = confirm(
+  "If you would like to include special characters(#@$%&*^!), click 'ok'."
+);
+var numChar = confirm(
+  "If you would like to include numeric characters, click 'ok'."
+);
+var lowChar = confirm(
+  "If you would like to include lowercase characters, click 'ok'."
+);
+var uppChar = confirm(
+  "If you would like to include uppercase characters, click 'ok'."
+);
+var possChar = [];
+if (specialChar) possChar.push("specialChar");
+if (numChar) possChar.push("numChar");
+if (lowChar) possChar.push("lowChar");
+if (uppChar) possChar.push("uppChar");
+
+var password = "";
+
+for(let i = 0; i <= charLength.length; i++) {
+  var groupIndex = Math.floor(Math.random * possChar.length);
+  var group = null
+  if(possChar[groupIndex] === "specialChar"){
+    group = specialChar;
+  }
+
+  for(let i = 0; i <= charLength.length; i++) {
+    var groupIndex = Math.floor(Math.random * possChar.length);
+    var group = null
+    if(possChar[groupIndex] === "numChar"){
+      group = numChar;
+}
+
+for(let i = 0; i <= charLength.length; i++) {
+  var groupIndex = Math.floor(Math.random * possChar.length);
+  var group = null
+  if(possChar[groupIndex] === "lowChar"){
+    group = lowChar;
+  }
+
+    for(let i = 0; i <= charLength.length; i++) {
+      var groupIndex = Math.floor(Math.random * possChar.length);
+      var group = null
+      if(possChar[groupIndex] === "uppChar"){
+        group = uppChar;
+      }
+    }
 
 
 
@@ -54,7 +95,7 @@ var generatePassword = function(){
 // * You can create other functions that are called from within
 //   generatePassword
 //
-// * Gather user input with prompt's and confirm's    
+// * Gather user input with prompt's and confirm's
 
 ///////////////////////////////////////////////////////////////////////
 // DO NOT TOUCH THIS CODE
@@ -62,18 +103,18 @@ var generatePassword = function(){
 // This code handles:
 // * clicking the Generate Password
 // * writing the password to the screen
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
-// function writePassword() {
-  // var password = generatePassword()
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
 
-//   var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
-// }
+  passwordText.value = password;
+}
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 //////////////////////////////////////////////////////////////////////
